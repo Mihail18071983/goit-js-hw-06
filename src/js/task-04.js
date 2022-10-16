8dce53410277a5ee4ff096716532ac1aac55322c
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-10-16 17:44:32 +0300

    Update task-04.js

---
+++ src/js/task-04.js
@@ -0,0 +1,17 @@
+let counterValue = 0;
+const decrBtn = document.querySelector('[data-action="decrement"]');
+const inkrBtn = document.querySelector('[data-action="increment"]');
+const value = document.querySelector('#value')
+console.log(decrBtn);
+console.log(inkrBtn);
+console.log(value);
+
+decrBtn.addEventListener('click', () => {
+    counterValue -= 1;
+    value.textContent = counterValue;
+})
+
+inkrBtn.addEventListener('click', () => {
+    counterValue += 1;
+    value.textContent = counterValue;
+})
