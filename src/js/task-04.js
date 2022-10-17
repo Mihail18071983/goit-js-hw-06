3a6b84f5bd7674b005dd1acafb1414162b1dac95
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-10-17 09:48:46 +0300

    update task4. Make task5

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -2,9 +2,7 @@ let counterValue = 0;
 const decrBtn = document.querySelector('[data-action="decrement"]');
 const inkrBtn = document.querySelector('[data-action="increment"]');
 const value = document.querySelector('#value')
-console.log(decrBtn);
-console.log(inkrBtn);
-console.log(value);
+
 
 decrBtn.addEventListener('click', () => {
     counterValue -= 1;
